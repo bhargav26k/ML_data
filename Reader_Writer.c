fe9a2b8ff1c8f36c3fbe6cdb47c426781397dc5d
Bhargav Karlapudi <bhargav2691@gmail.com>
2022-11-22 01:17:07 +0530

    Update Reader_Writer.c

--- Reader_Writer.c
+++ Reader_Writer.c
@@ -1,21 +1,14 @@
 #include <semaphore.h>
-
 #include<stdio.h>
-
 #include<stdlib.h>
-
 #include<pthread.h>
-
 sem_t x,y;
 
 pthread_t tid;
-
 pthread_t writerthreads[100],readerthreads[100];
-
 int readercount;
 
 void *reader(void *param)
-
 {
 
     sem_wait(&x);
@@ -31,11 +24,9 @@ void *reader(void *param)
     printf("\n%d reader is inside", readercount);
 
     sem_wait(&x);
-
     readercount--;
 
     if (readercount == 0)
-
     {
 
         sem_post(&y);
@@ -49,15 +40,10 @@ void *reader(void *param)
 }
 
 void *writer(void *param)
-
 {
-
     printf("\nWriter is trying to enter");
-
     sem_wait(&y);
-
     printf("\nWriter has entered");
-
     sem_post(&y);
 
     printf("\nWriter is leaving");
@@ -65,7 +51,6 @@ void *writer(void *param)
 }
 
 int main()
-
 {
 
     int n2, i;
@@ -73,27 +58,18 @@ int main()
     printf("Enter the number of readers:");
 
     scanf("%d", &n2);
-
     int n1[n2];
-
     sem_init(&x, 0, 1);
-
     sem_init(&y, 0, 1);
-
     for (i = 0; i < n2; i++)
 
     {
 
         pthread_create(&writerthreads[i], NULL, reader, NULL);
-
         pthread_create(&readerthreads[i], NULL, writer, NULL);
-
     }
-
     for (i = 0; i < n2; i++)
-
     {
-
         pthread_join(writerthreads[i], NULL);
 
         pthread_join(readerthreads[i], NULL);
